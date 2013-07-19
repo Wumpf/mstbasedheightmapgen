214d0cde3e26d17dbc7146eba720550741dd2eb5
Johannes Jendersie <jojendersie@web.de>
2013-07-19 20:36:37 +0200

    new clr interface for the new class * added missing file Refraction.cs

--- mst based heightmap/mst based heightmap.h
+++ mst based heightmap/mst based heightmap.h
@@ -4,6 +4,7 @@ using namespace System::Runtime::InteropServices;
 
 using namespace System;
 class HeightmapFactory;
+class GeneratorPipeline;
 
 namespace MstBasedHeightmap {
 
@@ -32,6 +33,26 @@ namespace MstBasedHeightmap {
 
 	private:
 		::HeightmapFactory* _nativeHeightmapFactory;
+	};
+
+	/// \brief Maps the exported functions of the GeneratorPipeline to a clr
+	///		interface.
+	public ref class GeneratorPipeline
+	{
+	public:
+		GeneratorPipeline(String^ jsonCode);
+		~GeneratorPipeline();
+
+		/// \brief Fills a buffer of arbitrary size with the use of the
+		///		current commands.
+		/// \details The size of the array determines the sampling only. The
+		///		shown sector of the map is defined by the json-command file
+		///		during construction.
+		/// \param [out] outData The array which is filled with the results.
+		///		The size defines the sampling of the map.
+		void Execute(array<float, 2>^ outData);
+	private:
+		::GeneratorPipeline* _nativeGenerator;
 	};
 }
 
