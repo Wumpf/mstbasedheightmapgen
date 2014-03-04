53e95e361760c4b0d2b4aeb1cd7efb8cb68af2ff
Andreas Reich <@>
2014-03-04 22:51:28 +0100

    Better ShaderAutoReload - however, it behaves very strange (looks like I'am atm blind to some very important detail...)

--- MST Heightmap Generator GUI/ShaderAutoReload.cs
+++ MST Heightmap Generator GUI/ShaderAutoReload.cs
@@ -1,6 +1,7 @@
 ﻿using SharpDX.Toolkit.Graphics;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,39 +16,48 @@ namespace MST_Heightmap_Generator_GUI
         public event Action OnReload;
 
         private string shaderFilename;
-        private DateTime lastShaderLoadTime;
         private GraphicsDevice graphicsDevice;
-        private Timer checkReloadTimer;
         private System.Windows.Threading.Dispatcher dispatcher;
+
+        private FileSystemWatcher watcher;
 
         private List<EffectData.ShaderMacro> shaderMacros;
 
         public ShaderAutoReload(String shaderFilename, GraphicsDevice graphicsDevice, List<EffectData.ShaderMacro> shaderMacros = null, double checkEvery_ms = 500.0)
         {
-            this.shaderFilename = shaderFilename;
+            this.shaderFilename = Path.GetFullPath(shaderFilename);
             this.graphicsDevice = graphicsDevice;
             this.shaderMacros = shaderMacros;
             ReloadShader();
 
             dispatcher = System.Windows.Threading.Dispatcher.CurrentDispatcher;
-            checkReloadTimer = new Timer(checkEvery_ms);
-            checkReloadTimer.Elapsed += (a, b) => ReloadIfNecessary();
-            checkReloadTimer.Enabled = true;
+
+            watcher = new FileSystemWatcher();
+            watcher.Path = Path.GetFullPath(Path.GetDirectoryName(shaderFilename));
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+       | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Changed += OnSourceFileChanged;
+            watcher.Filter = "*.*"; // For unknown reason *.fx didn't work. The documentation says that it should be possible to set the specific file as filter http://msdn.microsoft.com/en-us/library/system.io.filesystemwatcher%28VS.71%29.aspx
+
+            watcher.EnableRaisingEvents = true;
         }
 
-        public void ReloadIfNecessary()
+        private void OnSourceFileChanged(object source, FileSystemEventArgs e)
         {
-            System.IO.FileInfo fileInfo = new System.IO.FileInfo(shaderFilename);
-            if (fileInfo.LastWriteTimeUtc > lastShaderLoadTime)
-                dispatcher.Invoke(ReloadShader);
+            if (e.FullPath.Contains(shaderFilename)) // For unknown reason there are a lot of temp files that start with the full path and have some characters at the end of the actual filename.
+            {
+                try
+                {
+                    dispatcher.Invoke(ReloadShader);
+                }
+                catch(Exception) {    } // Embarrassingly I also have to admit that I have AGAIN no clue what is going on here. There are plenty of NullPtr exceptions but ignoring works fine. >.<
+            }
         }
 
         private void ReloadShader()
         {
             if(Effect != null)
                 Effect.Dispose();
-
-            lastShaderLoadTime = DateTime.UtcNow;
 
             EffectCompilerFlags compilerFlags = EffectCompilerFlags.None;
 #if DEBUG
